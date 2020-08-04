80f66a96841f764e304fff85cae267e0419f59a8
80andAbove <39066219+80andAbove@users.noreply.github.com>
2020-08-05 00:21:22 +0900

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-^_i#z+)4025wiaaf^lhhs@k%t2haad7ytjv!n315!!(=))chi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dennis-ivy.herokuapp.com/', 'dennis-ivy.com, '127.0.0.1']
+ALLOWED_HOSTS = ['dennis-ivy.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
